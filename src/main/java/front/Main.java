a4079885bdd0206cf9fd79e07ff8d7fd680bb88e
Laptop <adammolczan@wp.pl>
2016-02-26 21:04:36 +0100

    rozpoczęcie

---
+++ src/main/java/front/Main.java
@@ -0,0 +1,14 @@
+package front;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String args []){
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        WelcomePanel welcomePanel = (WelcomePanel) context.getBean("welcome");
+
+        welcomePanel.getMessage();
+    }
+}
---
+++ src/main/java/front/WelcomePanel.java
@@ -0,0 +1,13 @@
+package front;
+
+public class WelcomePanel {
+    private String msg;
+
+    public void setMessage(String msg){
+        this.msg=msg;
+    }
+    
+    public String getMessage(){
+        return msg;
+    }
+}
